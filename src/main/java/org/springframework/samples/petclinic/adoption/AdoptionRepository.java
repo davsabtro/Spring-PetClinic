9b421759959c87fd97e3c4e688e85c46464b5321
artrodase <artrodase@alum.us.es>
2022-03-30 17:41:13 +0200

    Feat: hecha la tarea A3.5-C.1

---
+++ src/main/java/org/springframework/samples/petclinic/adoption/AdoptionRepository.java
@@ -0,0 +1,8 @@
+package org.springframework.samples.petclinic.adoption;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface AdoptionRepository extends CrudRepository<Adoption, Integer>{
+
+}
